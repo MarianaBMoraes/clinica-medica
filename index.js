f9560967c1fcd4904abdde24635079c74a7b3774
Mariana <marianabmoraes.ef@gmail.com>
2024-06-25 11:50:41 -0300

    feat: código atualizado

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/* Criar um sistema que irá gerenciar as consultas de uma clínica médica.
-Como requisitos, nós teremos as seguintes funcionalidades:
-
-Adicionar uma nova consulta
-Listar todas as consultas
-Atualizar uma consulta existente
-Cancelar uma consulta */
-
 let consultas = [];
 
 let consulta = {
