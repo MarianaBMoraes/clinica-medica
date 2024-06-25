fe75ea426b359c5cc8855f33a55d7939c8b44c9a
Mariana <marianabmoraes.ef@gmail.com>
2024-06-25 08:51:54 -0300

    init: first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* Criar um sistema que irá gerenciar as consultas de uma clínica médica.
+Como requisitos, nós teremos as seguintes funcionalidades:
+
+Adicionar uma nova consulta
+Listar todas as consultas
+Atualizar uma consulta existente
+Cancelar uma consulta */
+
+let consultas = [];
+
+let consulta = {
+  paciente: "",
+  médico: "",
+  data: "",
+  horario: "",
+};
